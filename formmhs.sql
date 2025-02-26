467a7c44605dbbfb70048450bd1cc8121533f307
Pramudya76 <126577524+Pramudya76@users.noreply.github.com>
2025-02-26 10:49:49 +0700

    Add files via upload

---
+++ formmhs.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Feb 2025 pada 04.43
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `formmhs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mhs`
+--
+
+CREATE TABLE `mhs` (
+  `id` int(11) NOT NULL,
+  `NIM` varchar(255) NOT NULL,
+  `Nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mhs`
+--
+
+INSERT INTO `mhs` (`id`, `NIM`, `Nama`) VALUES
+(1, 'A11.2023.15414', 'Pramudya Putra Pratama');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mhs`
+--
+ALTER TABLE `mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mhs`
+--
+ALTER TABLE `mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
